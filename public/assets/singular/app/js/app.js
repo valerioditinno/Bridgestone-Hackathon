ac97d6abdba81d496cb599d0741af5e3eeafc0ae
e.daniele.marchetti@gmail.com <Officina3>
2017-08-29 14:56:21 +0200

    aggiunto gestione gps

--- public/assets/singular/app/js/app.js
+++ public/assets/singular/app/js/app.js
@@ -13,7 +13,7 @@ if (typeof $ === 'undefined') { throw new Error('This application\'s JavaScript 
 
 
 var App = angular.module('singular', ['ngRoute', 'ngAnimate', 'ngStorage', 'ngCookies', 'pascalprecht.translate', 'ui.bootstrap', 'ui.router', 'oc.lazyLoad', 'cfp.loadingBar', 'ui.utils'])
-  .run(["$rootScope", "$state", "$stateParams", '$localStorage', function ($rootScope, $state, $stateParams, $localStorage) {
+  .run(["$rootScope", "$state", "$stateParams", '$localStorage', '$cookies', function ($rootScope, $state, $stateParams, $localStorage, $cookies) {
     // Set reference to access them from any scope
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
@@ -42,23 +42,22 @@ var App = angular.module('singular', ['ngRoute', 'ngAnimate', 'ngStorage', 'ngCo
         topbar: 'bg-primary'
       }
     };
-
+    var globalcookie = JSON.parse($cookies.get('globals'));
     // User information
     $rootScope.user = {
-      name: 'Jimmie Stevens',
+      name: $cookies.get('globals') === undefined ? '' : globalcookie.currentUser.username,
       job: 'Developer',
       picture: 'app/img/user/02.jpg'
     };
-
   }
-  ]);
+]);
 
 // Application Controller
 // ----------------------------------- 
 
 App.controller('AppController',
-  ['$rootScope', '$scope', '$state', '$window', '$localStorage', '$timeout', 'toggleStateService', 'colors', 'browser', 'cfpLoadingBar', '$http', 'flotOptions', 'support',
-    function ($rootScope, $scope, $state, $window, $localStorage, $timeout, toggle, colors, browser, cfpLoadingBar, $http, flotOptions, support) {
+  ['$rootScope', '$scope', '$state', '$window', '$localStorage', '$timeout', '$location', 'toggleStateService', 'colors', 'browser', 'cfpLoadingBar', '$http', 'flotOptions', 'support',
+    function ($rootScope, $scope, $state, $window, $localStorage, $timeout, $location, toggle, colors, browser, cfpLoadingBar, $http, flotOptions, support) {
       "use strict";
 
       if (support.touch)
@@ -348,7 +347,8 @@ App.config(['$stateProvider', '$urlRouterProvider', '$controllerProvider', '$com
       })
       .state('page.login', {
         url: '/login',
-        templateUrl: 'app/pages/login.html'
+        templateUrl: 'app/pages/login.html',
+        controller: 'LoginController'
       })
       .state('page.register', {
         url: '/register',
@@ -3599,32 +3599,21 @@ App.factory('AuthenticationService', ['$http', '$cookies', '$rootScope', '$timeo
   service.Login = Login;
   service.SetCredentials = SetCredentials;
   service.ClearCredentials = ClearCredentials;
+  service.IsLogged = IsLogged;
 
   return service;
 
+  function IsLogged() {
+    var cookielogin = $cookies.get('globals');
+    return !(cookielogin === undefined); 
+  }
+
   function Login(username, password, callback) {
 
-    /* Dummy authentication for testing, uses $timeout to simulate api call
-     ----------------------------------------------
-    $timeout(function () {
-      var response;
-      UserService.GetByUsername(username)
-        .then(function (user) {
-          if (user !== null && user.password === password) {
-            response = { success: true };
-          } else {
-            response = { success: false, message: 'Username or password is incorrect' };
-          }
-          callback(response);
-        });
-    }, 1000);
-*/
-    /* Use this for real authentication
-     ----------------------------------------------*/
     $http.post('../loginSite', { Username: username, Password: password })
-        .success(function (response) {
-            callback(response);
-        });
+      .success(function (response) {
+        callback(response);
+      });
   }
 
   function SetCredentials(username, password) {
@@ -3654,88 +3643,113 @@ App.factory('AuthenticationService', ['$http', '$cookies', '$rootScope', '$timeo
 }]);
 
 
-    // Base64 encoding service used by AuthenticationService
-    var Base64 = {
+// Base64 encoding service used by AuthenticationService
+var Base64 = {
 
-        keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+  keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
 
-        encode: function (input) {
-            var output = "";
-            var chr1, chr2, chr3 = "";
-            var enc1, enc2, enc3, enc4 = "";
-            var i = 0;
+  encode: function (input) {
+    var output = "";
+    var chr1, chr2, chr3 = "";
+    var enc1, enc2, enc3, enc4 = "";
+    var i = 0;
 
-            do {
-                chr1 = input.charCodeAt(i++);
-                chr2 = input.charCodeAt(i++);
-                chr3 = input.charCodeAt(i++);
+    do {
+      chr1 = input.charCodeAt(i++);
+      chr2 = input.charCodeAt(i++);
+      chr3 = input.charCodeAt(i++);
 
-                enc1 = chr1 >> 2;
-                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-                enc4 = chr3 & 63;
+      enc1 = chr1 >> 2;
+      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+      enc4 = chr3 & 63;
 
-                if (isNaN(chr2)) {
-                    enc3 = enc4 = 64;
-                } else if (isNaN(chr3)) {
-                    enc4 = 64;
-                }
+      if (isNaN(chr2)) {
+        enc3 = enc4 = 64;
+      } else if (isNaN(chr3)) {
+        enc4 = 64;
+      }
 
-                output = output +
-                    this.keyStr.charAt(enc1) +
-                    this.keyStr.charAt(enc2) +
-                    this.keyStr.charAt(enc3) +
-                    this.keyStr.charAt(enc4);
-                chr1 = chr2 = chr3 = "";
-                enc1 = enc2 = enc3 = enc4 = "";
-            } while (i < input.length);
+      output = output +
+        this.keyStr.charAt(enc1) +
+        this.keyStr.charAt(enc2) +
+        this.keyStr.charAt(enc3) +
+        this.keyStr.charAt(enc4);
+      chr1 = chr2 = chr3 = "";
+      enc1 = enc2 = enc3 = enc4 = "";
+    } while (i < input.length);
 
-            return output;
-        },
+    return output;
+  },
 
-        decode: function (input) {
-            var output = "";
-            var chr1, chr2, chr3 = "";
-            var enc1, enc2, enc3, enc4 = "";
-            var i = 0;
+  decode: function (input) {
+    var output = "";
+    var chr1, chr2, chr3 = "";
+    var enc1, enc2, enc3, enc4 = "";
+    var i = 0;
 
-            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
-            var base64test = /[^A-Za-z0-9\+\/\=]/g;
-            if (base64test.exec(input)) {
-                window.alert("There were invalid base64 characters in the input text.\n" +
-                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
-                    "Expect errors in decoding.");
-            }
-            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+    // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+    var base64test = /[^A-Za-z0-9\+\/\=]/g;
+    if (base64test.exec(input)) {
+      window.alert("There were invalid base64 characters in the input text.\n" +
+        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+        "Expect errors in decoding.");
+    }
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 
-            do {
-                enc1 = this.keyStr.indexOf(input.charAt(i++));
-                enc2 = this.keyStr.indexOf(input.charAt(i++));
-                enc3 = this.keyStr.indexOf(input.charAt(i++));
-                enc4 = this.keyStr.indexOf(input.charAt(i++));
+    do {
+      enc1 = this.keyStr.indexOf(input.charAt(i++));
+      enc2 = this.keyStr.indexOf(input.charAt(i++));
+      enc3 = this.keyStr.indexOf(input.charAt(i++));
+      enc4 = this.keyStr.indexOf(input.charAt(i++));
 
-                chr1 = (enc1 << 2) | (enc2 >> 4);
-                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                chr3 = ((enc3 & 3) << 6) | enc4;
+      chr1 = (enc1 << 2) | (enc2 >> 4);
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+      chr3 = ((enc3 & 3) << 6) | enc4;
 
-                output = output + String.fromCharCode(chr1);
+      output = output + String.fromCharCode(chr1);
 
-                if (enc3 != 64) {
-                    output = output + String.fromCharCode(chr2);
-                }
-                if (enc4 != 64) {
-                    output = output + String.fromCharCode(chr3);
-                }
+      if (enc3 != 64) {
+        output = output + String.fromCharCode(chr2);
+      }
+      if (enc4 != 64) {
+        output = output + String.fromCharCode(chr3);
+      }
 
-                chr1 = chr2 = chr3 = "";
-                enc1 = enc2 = enc3 = enc4 = "";
+      chr1 = chr2 = chr3 = "";
+      enc1 = enc2 = enc3 = enc4 = "";
 
-            } while (i < input.length);
+    } while (i < input.length);
 
-            return output;
-        }
-    };
+    return output;
+  }
+};
 
+
+App.controller('LoginController', ['$scope', '$location', 'AuthenticationService',
+  function ($scope, $location, AuthenticationService) {
+    var vm = this;
+    $scope.login = login;
+
+    (function initController() {
+      // reset login status
+      AuthenticationService.ClearCredentials();
+    })();
+
+    function login() {
+    $scope.dataLoading = true;
+      AuthenticationService.Login($scope.username, $scope.password, function (response) {
+        if (response.success) {
+          AuthenticationService.SetCredentials($scope.username, $scope.password);
+          $location.path('/');
+        } else {
+          console.log(response.message);
+          $scope.dataLoading = false;
+        }
+      });
+    };
+  }
+]);
 
 /**=========================================================
  * Module: SupportService.js
