769bb530779bf54d6d27a184398b00cdbb62cdfa
e.daniele.marchetti@gmail.com <Officina3>
2017-09-04 14:15:48 +0200

    release 1

--- public/assets/singular/app/js/app.js
+++ public/assets/singular/app/js/app.js
@@ -2736,9 +2736,9 @@ function AngularTableController($scope, $filter, ngTableParams, $cookies, $http)
     for(var i = 0; i<response.length; i++){
       if(typeof response[i].Site ==='undefined' || !response[i].Site)
         data.push({sessionid: response[i].Timestamp, date: response[i].Created_date, 
-        link_x:'../oldsite/mappasessione.html?Username='+username+'&Session='+response[i].Timestamp+"Coord=x",
-        link_y:'../oldsite/mappasessione.html?Username='+username+'&Session='+response[i].Timestamp+"Coord=y",
-        link_z:'../oldsite/mappasessione.html?Username='+username+'&Session='+response[i].Timestamp+"Coord=z",
+        link_x:'../oldsite/mappasessione.html?Username='+username+'&Session='+response[i].Timestamp+"&Coord=x",
+        link_y:'../oldsite/mappasessione.html?Username='+username+'&Session='+response[i].Timestamp+"&Coord=y",
+        link_z:'../oldsite/mappasessione.html?Username='+username+'&Session='+response[i].Timestamp+"&Coord=z",
       origin:response[i].origin});
     }
   });
